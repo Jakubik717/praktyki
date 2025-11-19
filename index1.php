89a3cec0fec10de9eb1e8f07dda3e0466dc73c53
Jakubik717 <jakub.wol2711@gmail.com>
2025-11-19 14:27:40 +0100

    zadanie z tablicami i formularzem

---
+++ index1.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$liczba1 = $_GET["liczba1"];
+$liczba2 = $_GET["liczba2"];
+$liczba3 = $_GET["liczba3"];
+
+$liczby = array($liczba1,$liczba2,$liczba3);
+
+$najwieksza_liczba = max($liczby);
+$najmniejsza_liczba = min($liczby);
+
+echo " <p>Największa liczba: ".$najwieksza_liczba. "</p>";
+echo "<p>Najmniejsza liczba: ".$najmniejsza_liczba. "</p>";
+
+
+
+?>
