3f52a5a522091c9b3670a50a60c7b2250eefe8b4
Jakubik717 <jakub.wol2711@gmail.com>
2025-11-20 09:44:05 +0100

    Update index1.php
    już poprawione

--- index1.php
+++ index1.php
@@ -13,12 +13,13 @@ $najmniejsza_liczba = min($liczby);
 echo " <p>Największa liczba: ".$najwieksza_liczba. "</p>";
 echo "<p>Najmniejsza liczba: ".$najmniejsza_liczba. "</p>";
 
-echo "Liczby w odwrotnej kolejności";
-for($i=count($liczby)-1;  $i >= 0; $i--){
-  echo $liczby[$i]. "";
+echo "Liczby w odwrotnej kolejności: ";
+for($i=count($liczby) -1;  $i >= 0; $i-- ){
+  echo $liczby[$i]. " ";
 }
 
 
 
 
 ?>
+
