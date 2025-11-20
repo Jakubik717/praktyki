83391a9fb95af99155daaa8e3cdd39624b5b525b
Jakubik717 <jakub.wol2711@gmail.com>
2025-11-20 08:25:04 +0100

    Update index6.php

--- index6.php
+++ index6.php
@@ -2,10 +2,11 @@
 
 $miasto=$_POST['miasta'];
 if ($miasto ){
-    echo "Wybrano miasto: "($miasto);
+    echo "Wybrano miasto: ".$miasto;
 
 } else {
     echo "musisz wybrać miasto z listy";
 }
 ?>
+
 
