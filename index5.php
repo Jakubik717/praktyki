def16d9063aeac06bbc19c2b9642a1eab6b3730a
Jakub <kubus@localhost>
2025-11-23 10:24:40 +0100

    changed title

--- index5.php
+++ index5.php
@@ -2,15 +2,10 @@
 
 if(isset($_POST["wiek"])){
     $wiek=$_POST["wiek"];
-    $wiek=123;
     if(filter_var($wiek, FILTER_VALIDATE_INT)){
         echo "wartość jest liczbą całkowitą"
     } else {
         echo "wartość nie jest liczbą całkowitą"
     }
 }
-
-
-
-
 ?>
