3e627d4a4d21a270a67ef38eb56735a8cbed68a8
Jakub <kubus@localhost>
2025-12-09 16:13:46 +0100

    connection_task

---
+++ lekcja15/index2.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Informacje o serwerze</h1>";
+
+echo "Nazwa serwera: ".$_SERVER["SERVER_NAME"]. "<br>";
+echo "Adres IP serwera: ".$_SERVER["SERVER_ADDR"]. "<br>";
+echo "Ścieżka do pliku: ".$_SERVER["SCRIPT_FILENAME"]. "<br>";
