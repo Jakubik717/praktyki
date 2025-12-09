6c9748cb0a1357606a6bb8e12b842d9d07ea268c
Jakub <kubus@localhost>
2025-12-09 13:48:53 +0100

    changes

--- lekcja15/time.php
+++ lekcja15/time.php
@@ -12,3 +12,11 @@ echo "<hr>";
 
 $rok_temu = time() - (365*24*60*60);
 echo date("l", $rok_temu);
+
+echo "<hr>";
+
+$now=new DateTime();
+$interval=new DateInterval("P1Y");
+$year_ago = date_sub($now, $interval);
+echo $year_ago->format("Y-m-d H:i"). "<br>";
+echo $year_ago->format("l");
