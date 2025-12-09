fafbd9f755b4b30b1e12323d45efae192f0bb3af
Jakub <kubus@localhost>
2025-12-09 17:17:51 +0100

    reworks

--- lekcja15/index2.php
+++ lekcja15/index2.php
@@ -1,6 +1,11 @@
 <?php
-echo "<h1>Informacje o serwerze</h1>";
+$address="";
 
+if (isset($_SERVER["SERVER_ADDR"])) {
+    $address=$_SERVER["SERVER_ADDR"];
+}
+
+echo "<h1>Informacje o serwerze</h1>";
 echo "Nazwa serwera: ".$_SERVER["SERVER_NAME"]. "<br>";
-echo "Adres IP serwera: ".$_SERVER["SERVER_ADDR"]. "<br>";
+echo "Adres IP serwera: ".$address. "<br>";
 echo "Ścieżka do pliku: ".$_SERVER["SCRIPT_FILENAME"]. "<br>";
