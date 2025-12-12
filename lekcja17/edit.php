c64fc9025ab6ab20e12a9cc3831a2003f0582215
Jakub <kubus@localhost>
2025-12-12 08:12:58 +0100

    change

--- lekcja17/edit.php
+++ lekcja17/edit.php
@@ -1,24 +1,22 @@
 <?php
 if (
-    isset($_GET['submit-edit']) &&
     isset($_GET['edit-id']) &&
     isset($_GET['first-name']) &&
     isset($_GET['last-name']) &&
     isset($_GET['school-class']) &&
     isset($_GET['grade-average'])
 ) {
-    $id = $_GET['edit-id'];
-    $first = $_GET['first-name'];
-    $last = $_GET['last-name'];
-    $class = $_GET['school-class'];
-    $avg = $_GET['grade-average'];
-
     $connection = mysqli_connect("localhost", "root", "", "laczenie");
 
     $query = "UPDATE students 
               SET first_name=?, last_name=?, school_class=?, grade_average=? 
               WHERE id=?";
-    mysqli_execute_query($connection, $query, [$first, $last, $class, $avg, $id]);
+
+    mysqli_execute_query(
+        $connection,
+        $query,
+        [$_GET['first-name'], $_GET['last-name'], $_GET['school-class'], $_GET['grade-average'], $_GET['edit-id']]
+    );
 
     mysqli_close($connection);
 
@@ -27,12 +25,13 @@ if (
 }
 
 $edit_data = null;
+
 if (isset($_GET['edit'])) {
-    $edit_id = $_GET['edit'];
     $connection = mysqli_connect("localhost", "root", "", "laczenie");
 
     $query = "SELECT * FROM students WHERE id=?";
-    $result = mysqli_execute_query($connection, $query, [$edit_id]);
+    $result = mysqli_execute_query($connection, $query, [$_GET['edit']]);
+
     $edit_data = mysqli_fetch_assoc($result);
 
     mysqli_close($connection);
--- lekcja17/insert.php
+++ lekcja17/insert.php
@@ -1,21 +1,21 @@
 <?php
 if (
-    isset($_GET['submit']) &&
     isset($_GET['first-name']) &&
     isset($_GET['last-name']) &&
     isset($_GET['school-class']) &&
-    isset($_GET['grade-average'])
+    isset($_GET['grade-average']) &&
+    !isset($_GET['edit-id'])
 ) {
-    $first = $_GET['first-name'];
-    $last = $_GET['last-name'];
-    $class = $_GET['school-class'];
-    $avg = $_GET['grade-average'];
-
     $connection = mysqli_connect("localhost", "root", "", "laczenie");
 
     $query = "INSERT INTO students (first_name, last_name, school_class, grade_average) 
               VALUES (?, ?, ?, ?)";
-    mysqli_execute_query($connection, $query, [$first, $last, $class, $avg]);
+
+    mysqli_execute_query(
+        $connection,
+        $query,
+        [$_GET['first-name'], $_GET['last-name'], $_GET['school-class'], $_GET['grade-average']]
+    );
 
     mysqli_close($connection);
 
